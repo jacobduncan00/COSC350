ef963730e3aace9d4f1aba772c748c444c05550c
Jacob Duncan <jacobduncan00@yahoo.com>
2020-10-02 08:46:47 -0400

    finished task 2

---
+++ Lab04/task1.c
@@ -0,0 +1,82 @@
+//Jacob Duncan
+//Sept 29, 2020
+//lab4
+//task1.c
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+//convert string to integer
+int convertToInt(char *str, int n);
+
+//convert integer to string
+int convertToStr(char *str, int x);
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    printf("[ERROR] Must pass one argument!\n");
+    exit(1);
+  }
+
+  int fd; //file descriptor
+
+  //open given file for read only
+  if ((fd = open(argv[1], O_RDONLY)) < 0)
+  {
+    printf("Open %s file error\n", argv[1]);
+    exit(2);
+  }
+
+  int size = lseek(fd, 0, SEEK_END); //get size of line in the file
+  lseek(fd, 0, SEEK_SET);            //reset cursor position
+
+  char buf, str[size];
+  int i = 0;
+
+  while ((read(fd, &buf, 1)) == 1)
+  {
+    if (buf >= '0' && buf <= '9') //if character read ranges 0-9 add to str
+    {
+      str[i] = buf;
+      i++;
+    }
+  }
+  close(fd);
+
+  int newSize = i;
+  char str2[newSize];
+
+  int x = convertToInt(str, newSize); //convert to int
+
+  x += 10; //add 10
+
+  convertToStr(str2, x); //convert to string
+
+  write(1, str2, i); //write to console
+  printf("\n");
+
+  return 0;
+}
+
+int convertToInt(char *str, int n)
+{
+  int num = 0;
+  for (int i = 0; i < n; i++)
+  {
+    num = num * 10 + str[i] - '0';
+  }
+
+  return num;
+}
+
+int convertToStr(char *str, int x)
+{
+  sprintf(str, "%d", x);
+  return (strlen(str));
+}
---
+++ Lab04/task2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int str_to_int(char *c)
+{
+  int a = 0;
+  int num = 0;
+  while (c[a] != '\0')
+  {
+    num = 10 * num + (c[a] - '0');
+    a++;
+  }
+  return num;
+}
+
+void asciiToChar(int fd, char a[])
+{
+  int ascii = str_to_int(a);
+  char temp = ascii;
+  write(fd, &temp, 1);
+}
+
+int main(int argc, char *argv[])
+{
+  int curr = 0;
+  char buf1[1], buf2[4];
+  if (argc != 2)
+  {
+    printf("[ERROR] Must pass one argument, the output file of task8 for lab3!\n");
+    exit(1);
+  }
+  int inputFile = open(argv[1], O_RDONLY);
+  if (inputFile < 0)
+  {
+    printf("ERROR: Input file error!");
+    exit(1);
+  }
+  int outputFile = open("task2output", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP);
+  if (outputFile < 0)
+  {
+    printf("ERROR: Output file error!");
+    return 0;
+  }
+  while (read(inputFile, buf1, 1) > 0)
+  {
+    if (buf1[0] == ' ' || buf1[0] == '\n')
+    {
+      buf2[curr] = '\0';
+      asciiToChar(outputFile, buf2);
+      curr = 0;
+    }
+    else
+    {
+      buf2[curr] = buf1[0];
+      curr++;
+    }
+  }
+  return 0;
+}
